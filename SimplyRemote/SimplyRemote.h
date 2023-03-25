5495203a2be0dd05b3780112eb0e241d6030efc3
developersparsh <sparsh.singh23@gmail.com>
2023-03-26 00:35:31 +0530

    Initial project setup

---
+++ SimplyRemote/SimplyRemote.h
@@ -0,0 +1,18 @@
+//
+//  SimplyRemote.h
+//  SimplyRemote
+//
+//  Created by Sparsh Singh on 26/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimplyRemote.
+FOUNDATION_EXPORT double SimplyRemoteVersionNumber;
+
+//! Project version string for SimplyRemote.
+FOUNDATION_EXPORT const unsigned char SimplyRemoteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplyRemote/PublicHeader.h>
+
+
